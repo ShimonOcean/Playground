78a2ae8b5f2538391e722a3a4224862ca0a7f371
ShimonIslam <shimon.ocean@gmail.com>
2022-01-24 15:04:27 -0800

    DFS for HTML document, brackets for HTML elements

--- GoLinkParser/parse.go
+++ GoLinkParser/parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a link (<a href="">) in HTML doc
 type Link struct {
@@ -10,5 +15,24 @@ type Link struct {
 
 // Parse takes in HTML doc, returns slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	dfs(doc, "")
 	return nil, nil
+}
+
+// Depth First Search through all nodes of html doc
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	// c = n's first child, if c is not nil continue to next sibling
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
