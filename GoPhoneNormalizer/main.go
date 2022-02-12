944252bc64d4b60e989a8fdc978ab4caf9a51adf
ShimonIslam <shimon.ocean@gmail.com>
2022-02-11 16:37:06 -0800

    Normalize w/ iteration and regex

--- GoPhoneNormalizer/main.go
+++ GoPhoneNormalizer/main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"bytes"
+	"regexp"
+)
+
 // Normalizes phone numbers
+// Iterate through string, catching all numerical characters and disposing of extra
 func normalize(phone string) string {
-	return phone
+	// bytes buffer is more efficient than string concatenation with +
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		if ch >= '0' && ch <= '9' {
+			// WriteRune: appends UTF-8 of input to buffer
+			buf.WriteRune(ch)
+		}
+	}
+
+	return buf.String()
+}
+
+func normalizeRegex(phone string) string {
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
 }
